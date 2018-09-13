5967b0c6d0a765c009a9a5003e6327c6f3805c08
Jose Garcia <josegarciamanez@gmail.com>
2018-09-13 19:01:16 +0200

    primer reducer

--- app1.ts
+++ app1.ts
@@ -1,1 +1,19 @@
-console.log('Hola Mundo');
+// Acciones (crear interface de la acción)
+interface Action {
+  type: string;
+  payload?: any;
+}
+
+// Crear función reducer que recibe el estado actual y la acción
+function reducer(state = 10, action: Action) {
+  if (action.type === "INCREMENTAR") {
+    return (state += 1);
+  }
+  return state;
+}
+
+// Usar el reducer (enviamos el estado como primer parámetro y la acción como segundo y retornamos el estado nuevo)
+const incrementadorAction: Action = {
+  type: "INCREMENTAR"
+};
+console.log(reducer(10, incrementadorAction));
